1263a447f63730c7420a74021d43828aee859062
Jean-Pierre Coetzee <jeanpierrec19@gmail.com>
2021-03-25 00:00:05 +0200

    Added environment and PEP8 compliance

--- loudnorm.py
+++ loudnorm.py
@@ -1,31 +1,28 @@
 import ffmpy
 import subprocess
 import os
-import json 
-iteration = 0
-print(len(os.listdir(".")), "Files Found")
+import json
+
 for i in os.listdir("."):
-    iteration += 1
-    print(iteration, ":")
     if not os.path.exists("processed"):
         os.makedirs("processed")
-    if not os.path.exists("processed/wavs"):
-        os.makedirs("processed/wavs")
-    if not os.path.exists("processed/txt"):
-        os.makedirs("processed/txt")
+        if not os.path.exists("processed/wavs"):
+            os.makedirs("processed/wavs")
+        if not os.path.exists("processed/txt"):
+            os.makedirs("processed/txt")
     outfilebase = "processed/" 
     outwav = outfilebase + "/wavs/" + os.path.splitext(i)[0] + ".wav"
     outtxt = outfilebase + "/txt/" + os.path.splitext(i)[0] + ".txt"
 
     if i[0] == "." or i[0] == "_" or i[-3:] == ".py" or i[-3:] == ".sh" or os.path.isdir(i) or i == "loudnorm":
         continue
-    print("Processing " + i)
+
     ff = ffmpy.FFmpeg(
-    global_options = ("-hide_banner -loglevel info"),
-    inputs={i: None},
-    outputs={'pipe:': '-f null -af loudnorm=print_format=json'}
+        global_options="-hide_banner -loglevel info",
+        inputs={i: None},
+        outputs={'pipe:': '-f null -af loudnorm=print_format=json'}
     )
-    none, loudstats  = ff.run(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    none, loudstats = ff.run(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     """index = loudstats.decode().find("loudnorm")
     for a in loudstats.decode()[index:]:
         if a == "{":
@@ -40,13 +37,15 @@ for i in os.listdir("."):
         index += 1
     loudstats = b"".join(loudstats[index:]) 
     loudstats = json.loads(loudstats)
-    outflags = '-f wav -af loudnorm=measured_I=' + loudstats['input_i'] + ":measured_TP=" + loudstats['input_tp'] + ":measured_LRA=" + loudstats['input_lra'] + ":measured_thresh=" + loudstats['input_thresh'] + ":offset=" + loudstats['target_offset'] + ":print_format=summary"
+    outflags = '-f wav -af loudnorm=measured_I=' + loudstats['input_i'] + ":measured_TP=" + loudstats['input_tp'] \
+               + ":measured_LRA=" + loudstats['input_lra'] + ":measured_thresh=" + loudstats['input_thresh'] \
+               + ":offset=" + loudstats['target_offset'] + ":print_format=summary"
     ff = ffmpy.FFmpeg(
-        global_options = ("-hide_banner -loglevel info -y"),
-        inputs={i:None},
+        global_options="-hide_banner -loglevel info -y",
+        inputs={i: None},
         outputs={outwav: outflags}
     )
-    test,loudstats2= ff.run(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    index=loudstats2.decode().find("[Parsed_loudnorm_0")
+    test, loudstats2 = ff.run(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    index = loudstats2.decode().find("[Parsed_loudnorm_0")
     loudstats2 = loudstats2.decode()[index:]
     open(outtxt, 'w').write(loudstats2)
